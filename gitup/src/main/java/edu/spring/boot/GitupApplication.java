4fd9c36b354e3d0f0155eb594ee797818f6c038d
pjh342 <iqqttti@naver.com>
2021-09-14 15:05:26 +0900

    3 commit

--- gitup/src/main/java/edu/spring/boot/GitupApplication.java
+++ gitup/src/main/java/edu/spring/boot/GitupApplication.java
@@ -8,7 +8,7 @@ public class GitupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitupApplication.class, args);
-		System.out.println("hello github");
+		System.out.println("hello github 수정합니다.");
 	}
 
 }
